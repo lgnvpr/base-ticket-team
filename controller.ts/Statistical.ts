ae5b758b3e31aee03d73b58f0779d66c7051a005
kim <ktdraft0902@gmail.com>
2020-12-23 18:08:30 +0700

    no change

--- controller.ts/Statistical.ts
+++ controller.ts/Statistical.ts
@@ -1,18 +1,18 @@
 
-export class IntervalTicketChart{
+export class IntervalTicketChart {
     day?: Date;
     data?: number;
 }
 
-export class Summary{
+export class Summary {
     totalCustomer?: number;
     totalRevenue?: number;
     totalTrip?: number;
-    totalTicket ?: number;
+    totalTicket?: number;
 }
 
-export class PropsSummary{
-    from : Date
-    to : Date
-    interval: "day"|"month"
+export class PropsSummary {
+    from?: Date
+    to?: Date
+    interval?: "day" | "month"
 }
